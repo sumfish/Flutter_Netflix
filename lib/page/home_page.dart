69a8fb486e6d57856a94f41f27022b345383accb
fishope <831upsummer@gmail.com>
2021-06-10 00:19:43 +0800

    reformat code

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -3,60 +3,57 @@ import 'package:flutter_netflix/widgets/header_video.dart';
 import 'package:flutter_netflix/widgets/preview.dart';
 
 class HomePage extends StatefulWidget {
-
   /// 記錄跳轉資訊
-  const HomePage({Key key}): super(key: key);
+  const HomePage({Key key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
-  GlobalKey<ScaffoldState> _globalKey = GlobalKey(debugLabel: "test scaffoldState");
+  GlobalKey<ScaffoldState> _globalKey =
+      GlobalKey(debugLabel: "test scaffoldState");
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _globalKey,
       backgroundColor: Colors.black,
-      body: CustomScrollView(
-            slivers: [
-              SliverAppBar(
-                floating: true,
-                pinned: false,
-                snap: false,
-                backgroundColor: Colors.transparent,
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text("TV Shows"),
-                    Text("Movies"),
-                    Text("My list"),
-                  ],
-                ),
-                textTheme: TextTheme(
-                  headline6: TextStyle(
-                    color: Colors.white, fontSize: 16.0
-                  )
-                ),
-                leading: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Image.asset("assets/Netflix_N.png"),
-                ),
-              ),
-              SliverList(
-                  delegate: SliverChildListDelegate([
-                    HeaderVideo(),
-                    Preview(title:'Previews', key: PageStorageKey('preview'),),
-                    _buildRecommendedMenu("Trending"),
-                    _buildRecommendedMenu("Recommedation"),
-                    SizedBox(
-                      height: 30.0,
-                    )
-                  ])
-              ),
-            ]),
+      body: CustomScrollView(slivers: [
+        SliverAppBar(
+          floating: true,
+          pinned: false,
+          snap: false,
+          backgroundColor: Colors.transparent,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text("TV Shows"),
+              Text("Movies"),
+              Text("My list"),
+            ],
+          ),
+          textTheme: TextTheme(
+              headline6: TextStyle(color: Colors.white, fontSize: 16.0)),
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset("assets/Netflix_N.png"),
+          ),
+        ),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          HeaderVideo(),
+          Preview(
+            title: 'Previews',
+            key: PageStorageKey('preview'),
+          ),
+          _buildRecommendedMenu("Trending"),
+          _buildRecommendedMenu("Recommedation"),
+          SizedBox(
+            height: 30.0,
+          )
+        ])),
+      ]),
     );
   }
 
@@ -77,38 +74,30 @@ class _HomePageState extends State<HomePage> {
           height: 150, //限制高度否則報錯
           child: ListView(
               scrollDirection: Axis.horizontal,
-              children: List.generate(8, (index){
+              children: List.generate(8, (index) {
                 return Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Ink( //inkwell外面要套ink 不是container
-                    width:100,
+                  child: Ink(
+                    //inkwell外面要套ink 不是container
+                    width: 100,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(8.0),
                         image: DecorationImage(
                             image: AssetImage("assets/videolist/ACE.jpeg"),
-                            fit: BoxFit.cover)
-                    ),
+                            fit: BoxFit.cover)),
                     child: InkWell(
                       borderRadius: BorderRadius.circular(8.0),
-                      onLongPress: (){
+                      onLongPress: () {
                         //呼叫不到
-                        _globalKey.currentState.showSnackBar(SnackBar(content: Text("動畫名字")));
+                        _globalKey.currentState
+                            .showSnackBar(SnackBar(content: Text("動畫名字")));
                       },
                     ),
                   ),
                 );
-              })
-          ),
+              })),
         )
       ],
     );
   }
-
 }
-
-
-
-
-
-
-
